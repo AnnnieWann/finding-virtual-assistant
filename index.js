bf0678cbb1626581cb84509d17ca0874f831340e
Annie <annie_wan1209@hotmail.com>
2023-02-21 11:07:12 +0800

    add search user function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const INDEX_URL = "https://user-list.alphacamp.io/api/v1/users/";
 let users = [];
+const filteredList = [];
 
 const dataPanel = document.querySelector(".data-panel");
+const submitButton = document.querySelector(".submit-button");
+const inputField = document.querySelector(".input-field");
 
 axios
   .get(INDEX_URL)
@@ -25,7 +28,7 @@ function renderUserList(list) {
         <div class="card-body">
             <p class="card-text">${user.name}</p>
         </div>
-        <button type="button" class="btn btn-sm btn-outline-info mb-2 more-button" data-toggle="modal" data-target="#user-modal" data-id=${user.id} >More</button>
+        <button type="button" class="btn btn-sm btn-outline-info mb-2more-button" data-toggle="modal" data-target="#user-modal" data-id=${user.id} >More</button>
         <button type="button" class="btn btn-sm btn-outline-info add-to-favourite">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
             <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
@@ -57,6 +60,24 @@ function renderUserModal(id) {
     <p>Region: ${users[id].region}</p>
   `;
 }
+
+function searchUser(input) {
+  for (let i = 0; i < users.length; i++) {
+    let name = users[i].name.toLowerCase();
+    if (name.includes(input)) {
+      filteredList.push(users[i]);
+    }
+  }
+  return filteredList;
+}
+
+submitButton.addEventListener("click", function onSubmitClicked(event) {
+  event.preventDefault();
+
+  const input = inputField.value.toLowerCase();
+  searchUser(input);
+  renderUserList(filteredList);
+});
 
 dataPanel.addEventListener("click", function onMoreButtonClicked(event) {
   if (event.target.classList.contains("more-button")) {
