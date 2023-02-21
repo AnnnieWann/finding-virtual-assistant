05de29d165efe7a72cc3d4c2d35c8b002e1afa89
Annie <annie_wan1209@hotmail.com>
2023-02-21 12:13:57 +0800

    add paginators function

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const INDEX_URL = "https://user-list.alphacamp.io/api/v1/users/";
+const NUM_PER_PAGE = 30;
+
 let users = [];
-const filteredList = [];
+const filteredUsers = [];
 
 const dataPanel = document.querySelector(".data-panel");
 const submitButton = document.querySelector(".submit-button");
 const inputField = document.querySelector(".input-field");
+const paginator = document.querySelector(".pagination");
 
-axios
-  .get(INDEX_URL)
-  .then(function (response) {
-    users = response.data.results;
-    let firstHalfUsers = users.slice(0, 100);
-    let secondHalfUsers = users.slice(101, 200);
-    renderUserList(firstHalfUsers);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+function getUsersByPage(page) {
+  let startIndex = (page - 1) * NUM_PER_PAGE;
+
+  return users.slice(startIndex, startIndex + NUM_PER_PAGE);
+}
 
 function renderUserList(list) {
   let HTMLContent = "";
@@ -28,7 +25,7 @@ function renderUserList(list) {
         <div class="card-body">
             <p class="card-text">${user.name}</p>
         </div>
-        <button type="button" class="btn btn-sm btn-outline-info mb-2more-button" data-toggle="modal" data-target="#user-modal" data-id=${user.id} >More</button>
+        <button type="button" class="btn btn-sm btn-outline-info mb-2 more-button" data-toggle="modal" data-target="#user-modal" data-id=${user.id} >More</button>
         <button type="button" class="btn btn-sm btn-outline-info add-to-favourite">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
             <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
@@ -65,10 +62,23 @@ function searchUser(input) {
   for (let i = 0; i < users.length; i++) {
     let name = users[i].name.toLowerCase();
     if (name.includes(input)) {
-      filteredList.push(users[i]);
+      filteredUsers.push(users[i]);
     }
   }
-  return filteredList;
+  return filteredUsers;
+}
+
+function renderPaginator(list) {
+  const data = filteredUsers.length > 0 ? filteredUsers : users;
+  let numOfPage = Math.ceil(data.length / NUM_PER_PAGE);
+  let HTMLContent = "";
+  for (let page = 1; page <= numOfPage; page++) {
+    HTMLContent += `
+      <li class="page-item"><a class="page-link" href="#" data-id=${page}>${page}</a></li>
+    `;
+  }
+
+  paginator.innerHTML = HTMLContent;
 }
 
 submitButton.addEventListener("click", function onSubmitClicked(event) {
@@ -76,7 +86,8 @@ submitButton.addEventListener("click", function onSubmitClicked(event) {
 
   const input = inputField.value.toLowerCase();
   searchUser(input);
-  renderUserList(filteredList);
+  renderUserList(filteredUsers);
+  renderPaginator(filteredUsers);
 });
 
 dataPanel.addEventListener("click", function onMoreButtonClicked(event) {
@@ -84,3 +95,18 @@ dataPanel.addEventListener("click", function onMoreButtonClicked(event) {
     renderUserModal(event.target.dataset.id - 1);
   }
 });
+
+paginator.addEventListener("click", function onPageClicked(event) {
+  renderUserList(getUsersByPage(event.target.dataset.id));
+});
+
+axios
+  .get(INDEX_URL)
+  .then(function (response) {
+    users = response.data.results;
+    renderUserList(getUsersByPage(1));
+    renderPaginator(users);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
